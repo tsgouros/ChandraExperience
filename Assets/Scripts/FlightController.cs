7e45c3f3b10f165878f7c99c3b18487c97447392
Alexander-Stewart <stewart.stewie@yahoo.com>
2019-10-27 20:30:38 -0400

    Flight controlled from right hand only.

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -13,6 +13,9 @@ public class FlightController : MonoBehaviour
 
     public float movementSpeed = 0.1f;
     public float turnSpeed = 0.1f;
+    private float SimulationRate = 60f;
+    public float RotationAmount = 1.5f;
+    private float RotationScaleMultiplier = 1.0f;
 
 
     // Start is called before the first frame update
@@ -27,42 +30,60 @@ public class FlightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MoveForwardBackward();
+
+        RotateLeftRight();
+    }
+
+    /*
+     * A function to handle moving backwards and forwards.
+     */
+    private void MoveForwardBackward()
+    {
         //  logic for moving camera rig forward/backward with left thumb stick.
-        if(OVRInput.Get(OVRInput.Button.PrimaryThumbstickUp, 
-                        OVRInput.Controller.LTouch) || 
-           OVRInput.Get(OVRInput.Button.PrimaryThumbstickDown, 
-                        OVRInput.Controller.LTouch)){
-            // The thumbstick state represented as a vector.
-            Vector2 thumbStickState = 
-                OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, 
-                             OVRInput.Controller.LTouch);
-
-            Vector3 faceForward = playerCenterEye.transform.forward;
-
-            if(thumbStickState.y > 0 || thumbStickState.y < 0) { // Moving forward.
-                Debug.Log("Moving...");
-                cameraRig.transform.position += 
-                    faceForward * movementSpeed * thumbStickState.y;
-            }
-        }
-
-        //  logic for turning camera rig with right thumb stick.
-        if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft,
-                        OVRInput.Controller.RTouch) ||
-            OVRInput.Get(OVRInput.Button.PrimaryThumbstickRight,
-                        OVRInput.Controller.RTouch))
+        if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickUp,
+                        OVRInput.Controller.LTouch) ||
+           OVRInput.Get(OVRInput.Button.PrimaryThumbstickDown,
+                        OVRInput.Controller.LTouch))
         {
             // The thumbstick state represented as a vector.
             Vector2 thumbStickState =
                 OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick,
-                             OVRInput.Controller.RTouch);
-            Vector3 lookVector = new Vector3(thumbStickState.x, 0, 0);
+                             OVRInput.Controller.LTouch);
 
-            Quaternion targetRotation = Quaternion.LookRotation(lookVector, Vector3.up);
-            cameraRig.transform.rotation = Quaternion.Lerp(cameraRig.transform.rotation, targetRotation, Time.deltaTime);
+            Vector3 faceForward = playerCenterEye.transform.forward;
 
+            if (thumbStickState.y > 0 || thumbStickState.y < 0)
+            { // Moving forward.
+                Debug.Log("Moving...");
+                cameraRig.transform.position +=
+                    faceForward * movementSpeed * thumbStickState.y;
+            }
         }
+    }
 
+    /*
+     * A function to handle rotating.
+     */
+    private void RotateLeftRight()
+    {
+        //  logic for turning camera rig with left thumb stick.
+        if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft,
+                        OVRInput.Controller.LTouch) ||
+            OVRInput.Get(OVRInput.Button.PrimaryThumbstickRight,
+                        OVRInput.Controller.LTouch))
+        {
 
+            Vector3 euler = transform.rotation.eulerAngles;
+            float rotateInfluence = SimulationRate * Time.deltaTime * RotationAmount * RotationScaleMultiplier;
+
+            // The thumbstick state represented as a vector.
+            Vector2 thumbStickState =
+                OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick,
+                             OVRInput.Controller.LTouch);
+
+            euler.y += thumbStickState.x * rotateInfluence;
+            transform.rotation = Quaternion.Euler(euler);
+        }
     }
 }
